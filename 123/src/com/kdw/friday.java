6b362b999e3001e724e3000f280f80732d3602c4
reinca0314 <asas6987@gmail.com>
2026-05-29 15:50:09 +0900

    Initial commit

---
+++ 123/src/com/kdw/friday.java
@@ -0,0 +1,15 @@
+package com.kdw;
+
+public class friday {
+
+	public static void main(String[] args) {
+		
+		int myNum = 5;
+		float myFloatNum = 5.99;
+		char meLetter = 'D;'
+		boolean myBool = true;
+		String myText = "hello";
+
+	}
+
+}
